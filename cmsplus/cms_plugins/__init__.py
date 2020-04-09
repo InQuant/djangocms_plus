fbe7c1296f8a3b0e27c69e54d001fff13ddbcc78
Lutz Mende <lutz.mende@inquant.de>
2020-04-09 07:54:52 +0200

    added needed base functionality to porting cascade, ported first cascade TextLinkPlugin (still w/o js)
    added todo removed comment for parent_classes in TextLinkPlugin

---
+++ cmsplus/cms_plugins/__init__.py
@@ -0,0 +1,11 @@
+from importlib import import_module
+from cms.plugin_pool import plugin_pool
+
+from cmsplus import app_settings as cmspluscfg
+
+# register all plugins configured cmsplus app_settings
+for plugin in cmspluscfg.PLUGINS:
+    mod_name, cls_name = plugin.rsplit('.', 1)
+    mod = import_module(mod_name)
+    cls = getattr(mod, cls_name)
+    plugin_pool.register_plugin(cls)
--- cmsplus/fields.py
+++ cmsplus/fields.py
@@ -1,6 +1,16 @@
 from abc import abstractmethod, ABC
-from django import forms
 
+from django import forms
+from django.contrib.admin.sites import site as admin_site
+from django.db.models.fields.related import ManyToOneRel
+
+from filer.models.filemodels import File as FilerFileModel
+from filer.fields.file import AdminFileWidget, FilerFileField
+from filer.models.imagemodels import Image as FilerImageModel
+from filer.fields.image import AdminImageWidget, FilerImageField
+
+
+from cms.models.pagemodel import Page
 
 class BaseFieldMixIn(ABC):
     @abstractmethod
@@ -26,3 +36,50 @@ class PlusModelChoiceField(forms.ModelChoiceField, BaseFieldMixIn):
 
     def deserialize_field(self, value):
         return self.queryset.get(pk=value)
+
+
+class PageChoiceIterator(forms.models.ModelChoiceIterator):
+    ''' sort pages by absolute url
+    '''
+    def __iter__(self):
+        pages = sorted(self.queryset.all(), key=lambda p: p.get_absolute_url())
+        for obj in pages:
+            yield self.choice(obj)
+
+class PageSearchField(PlusModelChoiceField):
+    iterator = PageChoiceIterator
+
+    def __init__(self, *args, **kwargs):
+        queryset = Page.objects.public()
+        try:
+            queryset = queryset.on_site(get_current_site())
+        except:
+            pass  # can happen if database is not ready yet
+        kwargs.setdefault('queryset', queryset)
+        super().__init__(*args, **kwargs)
+
+    def label_from_instance(self, obj):
+        ''' display value is the absolute url, sorted via iterator above
+        '''
+        return obj.get_absolute_url()
+
+
+class PlusFilerFileSearchField(PlusModelChoiceField):
+
+    def __init__(self,
+        queryset=FilerFileModel.objects.all(),
+        widget=AdminFileWidget(
+            ManyToOneRel(FilerFileField, FilerFileModel, 'id'), admin_site),
+        *args, **kwargs):
+
+        super().__init__(queryset=queryset, widget=widget, *args, **kwargs)
+
+class PlusFilerImageSearchField(PlusModelChoiceField):
+
+    def __init__(self,
+        queryset=FilerImageModel.objects.all(),
+        widget=AdminFileWidget(
+            ManyToOneRel(FilerImageField, FilerImageModel, 'id'), admin_site),
+        *args, **kwargs):
+
+        super().__init__(queryset=queryset, widget=widget, *args, **kwargs)
