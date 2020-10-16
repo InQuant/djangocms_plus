959b2322edd4a7fc2ff017d5eb47650617eb0bb4
Lutz Mende <lutz.mende@inquant.de>
2020-10-16 16:08:01 +0200

    fixed card form assigning, and style defs

--- cmsplus/cms_plugins/bootstrap/card.py
+++ cmsplus/cms_plugins/bootstrap/card.py
@@ -5,8 +5,8 @@ from cmsplus.forms import PlusPluginFormBase, get_style_form_fields
 from cmsplus.plugin_base import PlusPluginBase, StylePluginMixin
 
 
-class CardChildForm(PlusPluginFormBase):
-    STYLE_CHOICES = 'SLIDE_STYLES'
+class CardChildBaseForm(PlusPluginFormBase):
+    STYLE_CHOICES = ''
 
     element_id = forms.CharField(label=_('Element ID'), max_length=255, required=False)
     extra_style, extra_classes, label, extra_css = get_style_form_fields(STYLE_CHOICES)
@@ -16,29 +16,42 @@ class CardChildBase(PlusPluginBase):
     module = "Bootstrap"
     require_parent = True
     parent_classes = ['BootstrapCardPlugin']
-    form = CardChildForm
     allow_children = True
     render_template = 'cmsplus/bootstrap/card/wrapper.html'
 
 
+class CardHeaderForm(CardChildBaseForm):
+    STYLE_CHOICES = 'CARD_HEADER_STYLES'
+
+
 class BootstrapCardHeaderPlugin(StylePluginMixin, CardChildBase):
     name = _("Card Header")
+    form = CardHeaderForm
     default_css_class = 'card-header'
+
+
+class CardBodyForm(CardChildBaseForm):
+    STYLE_CHOICES = 'CARD_BODY_STYLES'
 
 
 class BootstrapCardBodyPlugin(StylePluginMixin, CardChildBase):
     name = _("Card Body")
     default_css_class = 'card-body'
+    form = CardBodyForm
+
+
+class CardFooterForm(CardChildBaseForm):
+    STYLE_CHOICES = 'CARD_FOOTER_STYLES'
 
 
 class BootstrapCardFooterPlugin(StylePluginMixin, CardChildBase):
     name = _("Card Footer")
     default_css_class = 'card-footer'
+    form = CardFooterForm
 
 
-class CardFormMixin(CardChildForm):
+class CardForm(CardChildBaseForm):
     STYLE_CHOICES = 'CARD_STYLES'
-    extra_style, extra_classes, label, extra_css = get_style_form_fields(STYLE_CHOICES)
 
 
 class BootstrapCardPlugin(StylePluginMixin, PlusPluginBase):
@@ -46,8 +59,8 @@ class BootstrapCardPlugin(StylePluginMixin, PlusPluginBase):
     Use this plugin to display a card with optional card-header and card-footer.
     """
     module = "Bootstrap"
-    form = CardFormMixin
     name = _("Card")
+    form = CardForm
     default_css_class = 'card'
     require_parent = False
     allow_children = True
